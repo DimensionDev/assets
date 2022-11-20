cef9c365ed0cfffe6954b54a45a191cb949001f5
Lantt <laanfor@gmail.com>
2022-11-20 22:49:27 +0800

    feat: command support multi-chain

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -99,7 +99,11 @@ var (
 		Use:   "async-token-list",
 		Short: "Async Mask token list",
 		Run: func(cmd *cobra.Command, args []string) {
-			handleAsyncTokenList()
+			chains := getSupportChains()
+
+			for chain, url := range chains {
+				handleAsyncTokenList(chain, url)
+			}
 		},
 	}
 )
